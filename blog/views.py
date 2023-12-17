88e9a639b12b42709869bee7c22a9aa12e71fc3e
Jackson Lorran <computadordb@gmail.com>
2023-12-17 00:39:01 -0300

    little changes

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def blog(request):
+  olha = {'text':'Olá blog',
+          'title':'blog'}
   print('Blog')
-  return render(request,'blog/index.html')
+  return render(request,'blog/index.html', olha)
   
 def exemplo(request):
+  olha = {'text':'olá exemplo', 'title':'exemplo'}
   print('Exemplo')
-  return render(request,'blog/exemplo.html')
+  return render(request,'blog/exemplo.html', olha)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home/index.html')
+    olha = {'text':'Um texto foda', 'title':'Home'}
+    return render(
+        request,
+        'home/index.html',
+        olha
+        )
