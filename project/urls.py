8b516fa38dfc1cfc55e5db380d5696352d328c52
Jackson Lorran <computadordb@gmail.com>
2023-12-18 22:58:26 -0300

    Teste

--- project/urls.py
+++ project/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', include('home.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('static/global/css/styles.css',)
 ]
